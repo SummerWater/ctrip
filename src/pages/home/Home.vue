ed386b83b73c2ac24e461358035e4666c26c3185
shuleiming <364216899@qq.com>
2018-05-18 19:12:37 +0800

    home hot doing...

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-hot></home-hot>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeHot from './components/Hot'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeHot
   }
 }
 </script>
---
+++ src/pages/home/components/Hot.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="hot">
+    <div class="hot-title">
+      <span class="title"><img class="mp-title-img" src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png" alt="本周热门榜单">本周热门榜单</span>
+      <span class="more">全部榜单></span>
+    </div>
+    <ul class="hot-wrapper">
+      <li class="hot-item" v-for="(item, index) in hotList" :key="index" :ref="'hot_' + index">
+        <span class="top">Top1</span>
+        <img :src="item.imgUrl" class="hot-img">
+        <p class="title">{{item.title}}</p>
+        <p class="price">
+          <span class="orange">{{item.price}}</span>起
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHot',
+  data () {
+    return {
+      hotList: [
+        {
+          title: '大连森林动物园',
+          price: '￥58',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1707/57/5729585581b6732fa3.water.jpg_150x150_88ed4437.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        },
+        {
+          title: '大连圣亚海洋世界',
+          price: '￥25',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_150x150_2c4b6f2f.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/mixins.styl"
+  .hot
+    margin-top .2rem
+    background-color #ffffff
+    .hot-title
+      padding .24rem .2rem
+      .title
+        font-size .32rem
+        .mp-title-img
+          display inline-block
+          vertical-align middle
+          margin-bottom .06rem
+          margin-right .1rem
+          width .30rem
+      .more
+        font-size .26rem
+        color #666
+        line-height .4rem
+        float right
+    .hot-wrapper
+      display flex
+      padding-bottom .2rem
+      overflow-x scroll
+      overflow-y hidden
+      .hot-item
+        margin-left .2rem
+        width 2rem
+        position relative
+        .top
+          position absolute
+          right -.2rem
+          top -.2rem
+          width .1rem
+          height .4rem
+        .hot-img
+          width 2rem
+          height 2rem
+        .title
+          ellipsis()
+          font-size .24rem
+          padding-top .1rem
+          padding-bottom .1rem
+          text-align center
+        .price
+          font-size .24rem
+          text-align center
+          .orange
+            font-size .28rem
+            color #ff8300
+</style>
